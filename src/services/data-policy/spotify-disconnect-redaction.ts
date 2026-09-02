8ed99bfdde7310e2586d20848697c460e8374927
Marcos Nascimento <nascimento@itscontrol.com.br>
2026-09-02 18:17:45 -0300

    gate6b: add Spotify disconnect redaction constants

---
+++ src/services/data-policy/spotify-disconnect-redaction.ts
@@ -0,0 +1,6 @@
+export const SPOTIFY_DISCONNECT_REDACTED_TEXT =
+  "[redacted after Spotify disconnect]";
+
+export const SPOTIFY_DISCONNECT_REDACTED_URI = "redacted:spotify";
+
+export const SPOTIFY_DISCONNECT_REDACTED_ID_PREFIX = "redacted:spotify:";
