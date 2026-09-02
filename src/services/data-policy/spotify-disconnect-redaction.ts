6c153053d4710048b764d7a91e2bcf8b4dfebd2f
Marcos Nascimento <nascimento@itscontrol.com.br>
2026-09-02 18:31:58 -0300

    gate6b: define generic notification redaction payload

--- src/services/data-policy/spotify-disconnect-redaction.ts
+++ src/services/data-policy/spotify-disconnect-redaction.ts
@@ -4,3 +4,6 @@ export const SPOTIFY_DISCONNECT_REDACTED_TEXT =
 export const SPOTIFY_DISCONNECT_REDACTED_URI = "redacted:spotify";
 
 export const SPOTIFY_DISCONNECT_REDACTED_ID_PREFIX = "redacted:spotify:";
+
+export const SPOTIFY_DISCONNECT_REDACTED_NOTIFICATION_TAG =
+  "sonoriza-operational-redacted";
