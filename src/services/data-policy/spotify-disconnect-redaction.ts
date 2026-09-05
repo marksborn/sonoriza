bde8a2f4e446612bca06ad5f7f9565fecaa2a5e6
Marcos Nascimento <nascimento@itscontrol.com.br>
2026-09-05 16:40:56 -0300

    feat: define Spotify disconnect redaction sentinels

---
+++ src/services/data-policy/spotify-disconnect-redaction.ts
@@ -0,0 +1,9 @@
+export const SPOTIFY_DISCONNECT_REDACTED_TEXT =
+  "[redacted after Spotify disconnect]";
+
+export const SPOTIFY_DISCONNECT_REDACTED_URI = "redacted:spotify";
+
+export const SPOTIFY_DISCONNECT_REDACTED_ID_PREFIX = "redacted:spotify:";
+
+export const SPOTIFY_DISCONNECT_REDACTED_NOTIFICATION_TAG =
+  "sonoriza-operational-redacted";
